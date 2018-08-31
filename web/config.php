25dc7e3549d122068bef05a68b8500f658c0f1bc
pqguanyinli <34409013+pqguanyinli@users.noreply.github.com>
2018-08-31 14:37:03 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -3,7 +3,7 @@ define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyARHJJDFtwb5wcnURwwJA3FQRrM16PF27c');
 define('GJ_CODE', 'hk');
 define('SITE_NAME', 'GuanYouTube');
-define('TITLENAME', 'GuanYouTube-4K');
+define('TITLENAME', 'GuanYouTube');
 define('EN2DEKEY', 'fengfenghuohuochuangjiuzhou');
 define('EMAIL', 'pqguanyinli@gmail.com');
 ?>
